f98513fe7986e08c120942967acd1459dfc5b227
boompay <curryisdrink.sb@gmail.com>
2019-08-04 23:08:45 +0300

    Use Hooks at Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,8 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { User } from "firebase";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -6,7 +10,6 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -15,9 +18,6 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import InfoIcon from '@material-ui/icons/Info';
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import { User } from "firebase";
 
 interface Props {
   user: User | null;
@@ -41,60 +41,54 @@ const styles = {
   },
 };
 
-class MyAppBar extends Component<Props> {
-  state = {
-    drawer: false,
+function MyAppBar(props: Props) {
+  const { user, classes } = props;
+  const [drawer, setDrawer] = useState(false);
+
+  const handleMenu = () => {
+    setDrawer(true);
   };
 
-  handleMenu = () => {
-    this.setState({ drawer:true });
+  const handleClose = () => {
+    setDrawer(false);
   };
 
-  handleClose = () => {
-    this.setState({ drawer:false });
-  };
-
-  logout = () => {
+  const logout = () => {
     console.log("logout");
     firebase.auth().signOut();
   };
 
-  render() {
-    const { classes } = this.props;
-    return (
-      <>
-        <div className={classes.root}>
-          <AppBar position="static">
-            <Toolbar>
-              <IconButton className={classes.menuButton} onClick={this.handleMenu} color="inherit" aria-label="Menu">
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" className={classes.grow}>
-                Firebase Rocks!
-              </Typography>
-              {this.props.user
-                ? <Button color="inherit" onClick={this.logout}>Logout</Button>
-                : <Button color="inherit" to={"/login"} component={Link}>Login</Button>
-              }
-            </Toolbar>
-          </AppBar>
-          <Drawer open={this.state.drawer} onClose={this.handleClose}>
-            <List>
-              <ListItem button to="/" component={Link}>
-                <ListItemIcon><HomeIcon /></ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItem>
-              <Divider />
-              <ListItem button to="/about" component={Link}>
-                <ListItemIcon><InfoIcon /></ListItemIcon>
-                <ListItemText primary="About" />
-              </ListItem>
-            </List>
-          </Drawer>
-        </div>
-      </>
-    );
-  }
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} onClick={handleMenu} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Firebase Rocks!
+          </Typography>
+          {user
+            ? <Button color="inherit" onClick={logout}>Logout</Button>
+            : <Button color="inherit" to={"/login"} component={Link}>Login</Button>
+          }
+        </Toolbar>
+      </AppBar>
+      <Drawer open={drawer} onClose={handleClose}>
+        <List>
+          <ListItem button to="/" component={Link}>
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <Divider />
+          <ListItem button to="/about" component={Link}>
+            <ListItemIcon><InfoIcon /></ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
 }
 
 export default withStyles(styles)(MyAppBar);
