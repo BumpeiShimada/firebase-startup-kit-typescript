f91025de7a5e3a0406c1b165d8f23cf73683be20
boompay <curryisdrink.sb@gmail.com>
2019-11-24 22:01:40 +0200

    Update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import theme from "./theme";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, MuiThemeProvider } from "@material-ui/core";
 import Home from './Home';
 import * as firebase from "firebase";
 import "firebase/firestore";
